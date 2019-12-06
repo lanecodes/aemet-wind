2abcb710416f161a068cf57dd3304a9cbc9fdf81
Andrew Lane <ajlane50@gmail.com>
2019-12-06 11:15:13 +0000

    Add AemetHttpError exception class
    Handles situations where the API tells us about a HTTP error
    without raising one in such a way that requests picks up on it.

--- aemet_wind/web.py
+++ aemet_wind/web.py
@@ -17,15 +17,35 @@ class APIResponse:
     metadata_url: str
 
 
+class AemetHttpError(Exception):
+    """Exception arising when dealing with AEMET API."""
+    def __init__(self, descripcion: str, estado: int) -> None:
+        self.descripcion = descripcion
+        self.estado = estado
+
+    def __repr__(self):
+        return (f'AemetHttpError(estado: {self.estado}, '
+                f'descripcion: {self.descripcion}')
+
+    def __str__(self):
+        return (f'HTTP error code: {self.estado}\n'
+                f'description: {self.descripcion}')
+
+
 def get_response(endpoint_url: str) -> APIResponse:
+    """Get a response from the AEMET API using the given endpoint URL.
+
+    If there is an error, the API will just send back some JSON stating that
+    there is an error, so requests.get(...).raise_for_status() won't raise a
+    requests.exceptions.HTTPError. Instead we actually parse the returned JSON,
+    and if it turns out there was an error we raise our own AemetHttpError
+    exception.
+    """
     r = requests.get(endpoint_url)
-    try:
-        r.raise_for_status()
-    except requests.exceptions.HTTPError:
-        logging.error(
-            f'HTTP error occurred while accessing endpoint {endpoint_url}'
-        )
-        raise
+    response = r.json()
+
+    if response['estado'] != 200:
+        raise AemetHttpError(response['descripcion'], response['estado'])
 
     return APIResponse(
         r.json()['descripcion'],
